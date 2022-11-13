47d83eae5510567a14da36ba30a8753f5aa31563
Nerd <72287783+AdrianBhp43@users.noreply.github.com>
2022-11-13 20:00:12 +0700

    Update V1

--- lib/config/api.dart
+++ lib/config/api.dart
@@ -1,5 +1,5 @@
 class Api {
-  static const baseUrl = 'http://192.168.43.94/api_money_record';
+  static const baseUrl = 'http://192.168.1.109/api_money_record';
 
   static const history = '$baseUrl/history';
   static const user = '$baseUrl/user';
