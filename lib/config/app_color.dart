eac45bd961319875b8da47a3507f5ea945cf926b
Maulana Imanulhaq <98022263+Fancyyy21@users.noreply.github.com>
2022-11-18 14:27:12 +0700

    logo

--- lib/config/app_color.dart
+++ lib/config/app_color.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColor{
-  static const primary = Color(0XFF000000);
+  static const primary = Color.fromARGB(255, 0, 0, 0);
   static const secondary = Color(0XFFfca311);
   static const bg = Color(0XFFe5e5e5);
   static const chart = Color(0XFFffffff);
