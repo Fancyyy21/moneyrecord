cc613d9c0c29aef4bf9eb574fc214761db041328
Maulana Imanulhaq <98022263+Fancyyy21@users.noreply.github.com>
2022-11-30 11:12:48 +0700

    logo color

--- lib/config/api.dart
+++ lib/config/api.dart
@@ -1,5 +1,5 @@
 class Api {
-  static const baseUrl = 'http://192.168.1.4/api_money_record';
+  static const baseUrl = 'http://192.168.51.114/api_money_record';
 
   static const history = '$baseUrl/history';
   static const user = '$baseUrl/user';
--- lib/config/app_color.dart
+++ lib/config/app_color.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AppColor{
-  static const primary = Color(0XFF000000);
+  static const primary = Color(0XFFE8562C);
   static const secondary = Color(0XFFfca311);
-  static const bg = Color(0XFFe5e5e5);
-  static const chart = Color(0XFFFF0000);
+  static const bg = Color(0XFFA2AED0);
+  static const chart = Color(0XFF1F3573);
 }
